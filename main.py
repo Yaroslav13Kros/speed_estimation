4f72718172a85a8e1e8f9c2ce3c9ffc7cffb5a63
Yaroslav13Kros <yakirilennko7@gmail.com>
2021-08-22 23:44:46 +0300

    Add GUI form for controling motor

--- main.py
+++ main.py
@@ -1,23 +1,57 @@
 import numpy as np
-import motor
-#################################
+from motor import Motor
+from tkinter import *
+import tkinter.font
+######################################################################################
 # L298N Module 1
-# front motor right En = Gpio
-# back motor right En = Gpio
-#################################
+# front motor right En1 = Gpio 22, In1 =  Gpio 27, In2 = Gpio 17 Speed sensor = 5 +
+# back  motor right En2 = Gpio 2,  In1 =  Gpio 3,  In2 = Gpio 4  Speed sensor = 10 +
+######################################################################################
 
-#################################
+######################################################################################
 # L298N Module 2
-# front motor left En = Gpio
-# back motor left En = Gpio
-#################################
+# front motor left En1 = Gpio 14, In1 =  Gpio 15, In2 = Gpio 18 Speed sensor = 11
+# back  motor left En2 = Gpio 25, In1 =  Gpio 24, In2 = Gpio 23 Speed sensor = 9
+######################################################################################
+front_right = Motor(22, 27, 17)
+back_right  = Motor(2, 3, 4)
+
+front_left  = Motor(25, 24, 23)
+back_left   = Motor(14, 15, 18)
+win = Tk()
+win.title("Motor control")
+myFont = tkinter.font.Font(family = "Hevvetica", size = 12, weight = "bold")
+#
+code = Entry(win, font = myFont, width = 10)
+code.grid(row = 2, column = 1)
+
+def motorEnable():
+    setSpeed = int(code.get())
+    front_right.moveF(setSpeed)
+
+def motorDisable():
+    front_right.stop()
+
+def close_win():
+    front_right.stop()
+    win.destroy()
+
 def main():
     print("main function")
-    front_right =
-    front_left  =
-    back_right  =
-    back_left   =
 
 
-if __name__ = "__main__":
+    EnableMotorBtn = Button(win, text = "Motor On", font = myFont, command = motorEnable, bg = 'bisque2', height = 1, width = 24)
+    EnableMotorBtn.grid(row = 0, column = 1)
+
+    DisableMotorBtn = Button(win, text = "Motor Off", font = myFont, command = motorDisable, bg = 'bisque2', height = 1, width = 24)
+    DisableMotorBtn.grid(row = 1, column = 1)
+
+
+
+    win.protocol("WM_DELETE_WINDOW", close_win)
+    win.mainloop()
+
+
+
+if __name__ == "__main__":
     main()
--- motor.py
+++ motor.py
@@ -2,16 +2,11 @@ import RPi.GPIO as GPIO
 from time import sleep
 from enum import Enum
 
-
-class DirectionType(Enum):
-    NO_DIRECTION = 0
-    FORWARD = 1
-    REVERSE = 2
-
-
-class Motor:
+class Motor():
     def __init__(self, ena, in1, in2):
         if isinstance(ena, int) and isinstance(in1, int) and isinstance(in2, int):
+            GPIO.setmode(GPIO.BCM)
+            GPIO.setwarnings(False)
             self.__Ena = ena
             self.__In1 = in1
             self.__In2 = in2
@@ -19,6 +14,19 @@ class Motor:
             GPIO.setup(self.__In1, GPIO.OUT)
             GPIO.setup(self.__In2, GPIO.OUT)
             self.__pwm = GPIO.PWM(self.__Ena, 100)
-            self.pwm.start(0)
+            self.__pwm.start(0)
         else:
             print("Error: ena, in1, int2 should be int")
+
+    def moveF(self, speed = 50):
+        GPIO.output(self.__In1, GPIO.LOW)
+        GPIO.output(self.__In2, GPIO.HIGH)
+        self.__pwm.ChangeDutyCycle(speed)
+
+    def moveB(self, speed = 50):
+        GPIO.output(self.__In1, GPIO.HIGH)
+        GPIO.output(self.__In2, GPIO.LOW)
+        self.__pwm.ChangeDutyCycle(speed)
+
+    def stop(self):
+        self.__pwm.ChangeDutyCycle(0)
